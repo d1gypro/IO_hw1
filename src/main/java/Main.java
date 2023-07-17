a05dba85d7b2b8b44e672bb6fca104f43cbad987
d1gypro <d1gypro@yandex.ru>
2023-07-17 20:40:11 +0300

    add metods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,37 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         StringBuilder log = new StringBuilder();
 
-        File dir1 = new File("D://games/src/main");
-        if (dir1.mkdirs())
-            log.append("Каталог D://games/src/main создан \n");
+        List<String> dirList = new ArrayList(Arrays.asList(
+                "D://games/src",
+                "D://games/src/main",
+                "D://games/res",
+                "D://games/savegames",
+                "D://games/temp",
+                "D://games/src/test",
+                "D://games/res/drawables",
+                "D://games/res/vectors",
+                "D://games/res/icons"));
 
-        dir1 = new File("D://games/res");
-        if (dir1.mkdir())
-            log.append("Каталог D://games/res создан \n");
+        List<String> fileList = new ArrayList(Arrays.asList(
+                "D://games/src/main/Main.java",
+                "D://games/src/main/Utils.java",
+                "D://games/temp/temp.txt"));
 
-        dir1 = new File("D://games/savegames");
-        if (dir1.mkdir())
-            log.append("Каталог D://games/savegames создан \n");
-
-        dir1 = new File("D://games/temp");
-        if (dir1.mkdir())
-            log.append("Каталог D://games/temp создан \n");
-
-        dir1 = new File("D://games/src/test");
-        if (dir1.mkdir())
-            log.append("Каталог D://games/src/test создан \n");
-
-        File file = new File("D://games/src/main/Main.java");
-        try {
-            if (file.createNewFile())
-                    log.append("Файл Main.java был создан \n");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        for (String dir : dirList) {
+            MakeDir(dir, log);
         }
 
-        file = new File("D://games/src/main/Utils.java");
-        try {
-            if (file.createNewFile())
-                log.append("Файл Utils.java был создан \n");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        if ((new File("D://games/res/drawables")).mkdir())
-            log.append("Каталог D://games/res/drawables создан \n");
-
-        if ((new File("D://games/res/vectors")).mkdir())
-            log.append("Каталог D://games/res/vectors создан \n");
-
-        if ((new File("D://games/res/icons")).mkdir())
-            log.append("Каталог D://games/res/icons создан \n");
-
-        try {
-            if (new File("D://games/temp/temp.txt").createNewFile())
-                log.append("Файл temp.txt был создан \n");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        for (String file : fileList) {
+            CreateFile(file, log);
         }
 
         try (FileOutputStream fos = new FileOutputStream("D://games/temp/temp.txt")) {
@@ -65,5 +40,26 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static boolean MakeDir(String dirPath, StringBuilder log) {
+        File dir = new File(dirPath);
+        if (dir.mkdir()) {
+            log.append("Каталог " + dirPath + " создан \n");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean CreateFile(String pathName, StringBuilder log) {
+        File file = new File(pathName);
+        try {
+            if (file.createNewFile()) {
+                log.append("Файл " + pathName + " был создан \n");
+                return true;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 }
